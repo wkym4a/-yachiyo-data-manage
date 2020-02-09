55af91a442eba3f69b40a2f8a9d3cddbbbd069b8
若山侑輝 <wkym_4a@icloud.com>
2020-02-09 12:31:23 +0900

    add_時間設定を東京に、エラーなどの表示言語を日本語に

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module YachiyoData
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    #タイムゾーン設定を東京に
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
